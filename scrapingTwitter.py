c30fe8341e4d258b69cf98878a02f0f2a500c7a8
Rabii <rabi3jouira@gmail.com>
2021-11-16 23:04:07 +0100

    Update scrapingTwitter.py

--- scrapingTwitter.py
+++ scrapingTwitter.py
@@ -6,29 +6,18 @@ import pandas as pd
 # you can get api twitter key from here : https://developer.twitter.com/en/apply-for-access
 
 consumer_key= '************your consumer_key**************'
-
 consumer_secret = '************your consumer_secret****************'
-
 access_token_secret='***********access_token_secret***************'
-
 access_secret='************ your access_secret ************'
 
- 
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
 auth.set_access_token(access_token_secret, access_secret)
-
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
 try:
-
     api.verify_credentials()
-
     print("Authentication OK")
-
 except:
-
     print("Error during authentication")
  
 #   ****************************
